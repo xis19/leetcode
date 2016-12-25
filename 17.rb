c4b2562b30160a92a45f14c562fef295a75d0f03
Xiaoge Su <magichp@gmail.com>
2016-12-25 01:30:24 -0800

    Solution to 17. Letter Combinations of a Phone Number

---
+++ 17.rb
@@ -0,0 +1,23 @@
+BUTTONS = {
+  '0' => ' ',
+  '2' => 'abc',
+  '3' => 'def',
+  '4' => 'ghi',
+  '5' => 'jkl',
+  '6' => 'mno',
+  '7' => 'pqrs',
+  '8' => 'tuv',
+  '9' => 'wxyz'
+}
+
+def letter_combinations(input)
+  return [] if input.empty?
+  construct = ->(digits) do
+    if digits.empty?
+      ['']
+    else
+      BUTTONS[digits[0]].chars.product(construct.call(digits[1..-1])).map { |i| i.join }
+    end
+  end
+  construct.call(input)
+end
