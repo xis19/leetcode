b483790b7f1b32261339f903293805abbf61e85e
Xiaoge Su <magichp@gmail.com>
2019-05-12 11:34:31 -0700

    Solution to 938. Range Sum of BST

---
+++ 938.cpp
@@ -0,0 +1,14 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+int rangeSumBST(TreeNode* root, int L, int R) {
+    if (root == nullptr) return 0;
+    if (root->val < L) return rangeSumBST(root->right, L, R);
+    if (root->val > R) return rangeSumBST(root->left, L, R);
+    return root->val + rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R);
+}
+
