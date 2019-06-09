f599bc617ab1ec8650b2092f687a043e233e256c
Xiaoge Su <magichp@gmail.com>
2019-06-08 18:54:11 -0700

    Solution to 1075. Project Employees I

---
+++ 1075.sql
@@ -0,0 +1,11 @@
+SELECT
+    project_id, ROUND(AVG(Employee.experience_years), 2) AS average_years
+FROM
+    Project
+JOIN
+    Employee
+ON
+    Project.employee_id = Employee.employee_id
+GROUP BY
+    Project.project_id
+;
